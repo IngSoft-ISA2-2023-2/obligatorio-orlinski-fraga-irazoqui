6e204009da249b9fd68da54dd7692c872e2937e0
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2023-10-20 17:23:24 -0300

    Change product price from float to decimal

--- codigo/backend/PharmaGo.Domain/Entities/Product.cs
+++ codigo/backend/PharmaGo.Domain/Entities/Product.cs
@@ -14,7 +14,7 @@ namespace PharmaGo.Domain.Entities
         public string Code { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public float Price { get; set; }
+        public decimal Price { get; set; }
         public bool Deleted { get; set; }
         public int Stock { get; set; } = 0;
         public Pharmacy? Pharmacy { get; set; }
--- codigo/backend/PharmaGo.WebApi/Models/In/ProductModel.cs
+++ codigo/backend/PharmaGo.WebApi/Models/In/ProductModel.cs
@@ -7,7 +7,7 @@ namespace PharmaGo.WebApi.Models.In
         public string Code { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public float Price { get; set; }
+        public decimal Price { get; set; }
         public string PharmacyName { get; set; }
 
         public Product ToEntity()
