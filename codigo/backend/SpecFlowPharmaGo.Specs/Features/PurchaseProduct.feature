3b5028ba7c15a91da1871cd16a21096158d3113f
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2023-10-23 12:33:56 -0300

    Create specflow purchaseproduct feature

---
+++ codigo/backend/SpecFlowPharmaGo.Specs/Features/PurchaseProduct.feature
@@ -0,0 +1,8 @@
+﻿Feature: PurchaseProduct
+
+A short summary of the feature
+
+Scenario: add product to cart succesfully
+	Given that I can see a product's details
+	When I click the add to cart button
+	Then the product is added to my cart
---
+++ codigo/backend/SpecFlowPharmaGo.Specs/StepDefinitions/PurchaseProductStepDefinition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowPharmaGo.Specs.StepDefinitions
+{
+    [Binding]
+    public class PurchaseProductStepDefinition
+    {
+        [Given(@"that I can see a product's details")]
+        public void GivenThatICanSeeAProductsDetails()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I click the add to cart button")]
+        public void WhenIClickTheAddToCartButton()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the product is added to my cart")]
+        public void ThenTheProductIsAddedToMyCart()
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
